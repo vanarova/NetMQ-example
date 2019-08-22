d2891270003562459732475a10d3adcb1296307b
vanarova <vanarova@gmail.com>
2019-08-23 00:10:52 +0200

    Adding new files"

---
+++ NETMQServer/NETMQClient/Program.cs
@@ -0,0 +1,78 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETMQClient
+{
+
+    //CLIENT
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Start response socket first..
+            //using (var responseSocket = new ResponseSocket("@tcp://*:5252"))
+
+
+            string message;
+            try
+            {
+                Random rand = new Random(1000);
+                bool status = true;
+                //Now start request socket & send msg
+                using (var requestSocket = new RequestSocket(">tcp://localhost:5252"))
+                {
+                    while (status)
+                    {
+                        Console.WriteLine("requestSocket : Sending 'Hello'");
+                        status = requestSocket.TrySendFrame("Hello");
+                        //Console.WriteLine("Send Status:" + status);
+                        //var message = responseSocket.ReceiveFrameString();
+                        //Console.WriteLine("responseSocket : Server Received '{0}'", message);
+                        //Console.WriteLine("responseSocket Sending 'World'");
+                        //responseSocket.SendFrame("World");
+
+                        if (status)
+                        {
+                            for (int i = 0; i < 3; i++)
+                            {
+                                System.Threading.Thread.Sleep(100);
+                                status = requestSocket.TryReceiveFrameString(out message);
+                                Console.WriteLine("requestSocket : Received '{0}'", message);
+                                if (status)
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            //Break
+                            break;
+                        }
+                        //wait to receive
+                        //Console.WriteLine("Will execute to receive");
+                        //Console.ReadKey();
+                
+                       
+                        
+                        //Console.WriteLine("All Done..");
+                        //Console.ReadKey();
+                        
+                        System.Threading.Thread.Sleep(rand.Next(100,1000));
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
---
+++ NETMQServer/NETMQServer/Program.cs
@@ -0,0 +1,63 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETMQServer
+{
+    class Program
+    {
+        /// SERVER
+        
+        static void Main(string[] args)
+        {
+            string message;
+            bool status;
+            using (var responseSocket = new ResponseSocket("@tcp://*:5252"))
+            //using (var requestSocket = new RequestSocket(">tcp://localhost:5252"))
+            {
+                //Console.WriteLine("requestSocket : Sending 'Hello'");
+                //requestSocket.SendFrame("Hello");
+
+                //Start and wait for messages
+                //Console.WriteLine("Start and wait for messages");
+                //Console.ReadLine();
+                while (true)
+                {
+                    status = responseSocket.TryReceiveFrameString(out message);
+
+                    if (status)
+                    {
+                        Console.WriteLine("responseSocket : Server Received '{0}'", message);
+                        status = responseSocket.TrySendFrame("World");
+                        if (status)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            //May happen that another frame arrives, before sending, may never happen this scenario.
+                            responseSocket.TrySkipFrame();
+                        }
+                    }
+                  
+                }
+                
+                //Console.WriteLine("responseSocket : Server Received '{0}'", message);
+                //Console.WriteLine("responseSocket Sending 'World'");
+
+                
+                //responseSocket.SendFrame("World");
+                //Console.WriteLine("Received executed, Sent reponse");
+                //Console.ReadLine();
+                ////message = requestSocket.ReceiveFrameString();
+                //Console.WriteLine("requestSocket : Received '{0}'", message);
+                //Console.WriteLine("All Done..");
+                //Console.ReadLine();
+            }
+        }
+    }
+}
